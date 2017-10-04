ba590adfdcded88307f877c9a4214321c334d455
Adwi Arifin <adwiarifin@gmail.com>
2017-10-04 18:19:21 +0700

    repair project files

--- Project1.dpr
+++ Project1.dpr
@@ -2,8 +2,10 @@ program Project1;
 
 uses
   Forms,
-  Unit1 in 'Unit1.pas' {Form1},
-  Unit2 in 'Unit2.pas' {Form2};
+  UnitDataModule in 'UnitDataModule.pas' {DataModuleAjendam},
+  UnitMain in 'UnitMain.pas' {FormMain},
+  UnitCariData in 'UnitCariData.pas' {FormCariData},
+  UnitHistoryChooser in 'UnitHistoryChooser.pas' {FormHistory};
 
 {$R *.res}
 
@@ -11,7 +13,9 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.Title := 'Ajendam - Program Verifikasi';
-  Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TFormCariData, FormCariData);
+  Application.CreateForm(TFormHistory, FormHistory);
+  Application.CreateForm(TDataModuleAjendam, DataModuleAjendam);
   Application.Run;
 end.
